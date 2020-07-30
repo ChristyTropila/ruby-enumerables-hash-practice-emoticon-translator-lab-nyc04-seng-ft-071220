f89f3a4de963b9f130c0336474783918b7dd43ea
Christy Tropila <ctropilalakebeverage@gmail.com>
2020-07-30 14:29:00 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,18 @@
 # require modules here
+require 'yaml'
 
-def load_library
+
+def load_library(file_path)
   # code goes here
+emoticon=YAML.load_file(file_path)
+new_hash ={}
+
+emoticon.each do |emo, lang|
+new_hash[emo] = {}
+  new_hash[emo][:english] = lang[0]
+  new_hash[emo][:japanese] = lang[1]
+end
+new_hash
 end
 
 def get_japanese_emoticon
