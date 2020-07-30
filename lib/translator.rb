22703fb2b834f10cb9dbab7b1d48a7277b83ba2d
Christy Tropila <ctropilalakebeverage@gmail.com>
2020-07-30 14:37:08 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,7 +21,7 @@ def get_japanese_emoticon(file_path, emoticon)
     lib[lang][:english] ==emoticon
   end
   emoticon ? lib[emoticon][:japanese] : "Sorry, that emoticon was not found"
-  
+
 end
 
 def get_english_meaning(file_path, emoticon)
@@ -30,5 +30,5 @@ def get_english_meaning(file_path, emoticon)
     lib[lang][:japanese] ==emoticon
   end
   emoticon ? emoticon: "Sorry, that emoticon was not found"
-    
+
 end
